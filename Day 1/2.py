55731980f84d2771474ca8df897e0f14df6227cf
Asad Faridi <76862712+asad278@users.noreply.github.com>
2023-03-12 20:33:06 +0530

    Delete 2.py

--- Day 1/2.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.stdin = open('inputf.in', 'r')
-sys.stdout = open('outputf.out', 'w')
-
-for _ in range(int(input())):
-    n = int(input())
-    arr = list(map(int, input().split()))
-    arr[:] = arr[-1:] + arr[:-1]
-    print(arr)
