ded0cb149e0981f346abfd2bb4b12799a747ebe7
Asad Faridi <76862712+asad278@users.noreply.github.com>
2023-03-14 17:41:17 +0530

    Nim Game

--- Day 1/9.py
+++ Day 1/9.py
@@ -4,4 +4,9 @@ sys.stdout = open('outputf.out', 'w')
 
 for _ in range(int(input())):
     n = int(input())
-    print("Player 1 won" if n%4 else "Player 2 won")
+#     print("Player 1 won" if n%4 else "Player 2 won")
+    arr = list(map(int, input().split()))
+    xor=0
+    for i in range(n):
+        xor ^= arr[i]
+    print("Player 1 won" if xor else "Player 2 won")
