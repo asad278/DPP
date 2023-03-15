c6296ffd3ee2e654a819234192c8c26139be3ad8
Asad Faridi <76862712+asad278@users.noreply.github.com>
2023-03-16 01:16:06 +0530

    n Dice and m Faces with Target x

---
+++ Day 5/4.py
@@ -0,0 +1,24 @@
+import sys
+sys.stdin = open('inputf.in', 'r')
+sys.stdout = open('outputf.out', 'w')
+
+def ways(n, i, x, dp):
+    if (x<0) or (n==0 and x!=0) or (n!=0 and x==0):
+        return 0
+    if n==0 and x==0:
+        return 1
+        
+    if dp[n][x]!=-1:
+        return dp[n][x]
+    
+    ans = 0
+    for i in range(1,m+1):
+        ans += ways(n-1,m,x-i,dp)
+    
+    dp[n][x] = ans
+    return dp[n][x]
+
+for _ in range(int(input())):
+    n, m, x = map(int, input().split())
+    dp = [[-1 for _ in range(x+2)] for _ in range(n+1)]
+    print(ways(n, m, x, dp))
