3c2fafe11d084acd7de6ba9f0c1911c9c3dd89f4
Asad Faridi <76862712+asad278@users.noreply.github.com>
2023-03-18 02:04:38 +0530

    Tree Traversal

---
+++ Day 6/1.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.data = key
+
+def preorder(node):
+    if node:
+        print(node.data, end=" ")
+        preorder(node.left)
+        preorder(node.right)
+
+def inorder(node):
+    if node:
+        inorder(node.left)
+        print(node.data, end=" ")
+        inorder(node.right)
+
+def postorder(node):
+    if node:
+        postorder(node.left)
+        postorder(node.right)
+        print(node.data, end=" ")
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+print(preorder(root))
+print(inorder(root))
+print(postorder(root))
